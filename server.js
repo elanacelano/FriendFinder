0e5448547458ee2e2ccd987674823a4d7782e1df
Elana Celano <elanacelano@gmail.com>
2017-06-29 16:16:56 -0400

    with help

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Your server.js file should require the basic npm packages we"ve used in class: express, body-parser and path		
+// Your server.js file should require the basic npm packages we"ve used in class: express, body-parser and path        
 //required pkgs
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -15,7 +15,36 @@ app.use(bodyParser.json({
     type: "application/vnd.api+json"
 }));
 
-var friendFinder = [{
-	
-	
-}]
+
+// In this blank space is where we will write our code.
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/public/home.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT);
+
+
+
+
+
