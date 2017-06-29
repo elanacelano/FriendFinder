a2600699f8e2e7060b79fe464435e741cf6cdf2d
Elana Celano <elanacelano@gmail.com>
2017-06-29 13:38:59 -0400

    a little more

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,54 @@
+// characters from Dogma and Chasing Amy for friendFinder app
 var friendFinder = [{
-	
+  routeName: "jay",
+  name: "Jay",
+}, {
+  routeName: "silentBob",
+  name: "Silent Bob",
+}, {
+  routeName: "rufus",
+  name: "Rufus",
+}, {
+  routeName: "loki",
+  name: "Loki",
+}, {
+  routeName: "johnDoeJersey",
+  name: "John Doe Jersey",		
+}, {
+  routeName: "serendipity",
+  name: "Serendipity",
+}, {
+  routeName: "bethany",
+  name: "Bethany",
+}, {
+  routeName: "Liz",
+  name: "Liz",
+}, {
+  routeName: "amy",
+  name: "Amy",
+}, {
+  routeName: "jane",
+  name: "Jane",		
+}
+}];
+
+// Routes
+// ===========================================================
+app.get("/", function(req, res) {
+  res.send("Welcome to the Friend Finder App!");
+});
+
+app.get("/:friends", function(req, res) {
+  var chosen = req.params.characters;
+
+  res.end();
+});
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
 	
 }]
